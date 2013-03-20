a39bafbfd2044211b20c9ec7e8e17eb362d12050
Toshihiro Takehara aka nouphet <nouphet.ratanavong@gmail.com>
2013-03-21 01:27:10 +0900

    first commit.

---
+++ get_vm_list.php
@@ -0,0 +1,62 @@
+<?php
+
+$xmlBase = 'xml';
+// $xmlBase = '/etc/libvirt/qemu';
+
+$vmList = array('vm_name', 'vm_name2');
+
+$configs = array();
+
+$valueNames = array('vcpu', 'memory', 'uuid');
+$fileValueNames = array('source');
+
+foreach ($vmList as $vm) {
+	echo 'read ' . $vm . "\n";
+
+	// 最終的に出力したい値を格納していく配列
+	$configs[$vm] = array();
+
+	// VMの設定ファイルのPath
+	$targetVmXmlPath = $xmlBase . '/' . $vm . '.xml';
+
+	foreach ($valueNames as $valueName) {
+		$configs[$vm][$valueName] = getConfigValue($valueName, $targetVmXmlPath);
+	}
+
+	foreach ($fileValueNames as $fileValueName) {
+		$configs[$vm][$valueName] = getConfigFileValue($fileValueName, $targetVmXmlPath);
+	}
+}
+
+function getConfigValue($name, $targetVmXmlPath) {
+	$value = `cat $targetVmXmlPath | grep $name`;
+
+	$value = str_replace("<$name>", '', $value);
+	$value = str_replace("</$name>", '', $value);
+	$value = trim($value, "\n");
+	$value = trim($value, ' ');
+
+	return $value;
+}
+
+function getConfigFileValue($name, $targetVmXmlPath) {
+	$value = `cat $targetVmXmlPath | grep $name | grep file`;
+
+	$value = str_replace("<$name>", '', $value);
+	$value = str_replace("</$name>", '', $value);
+	$value = trim($value, "\n");
+	$value = trim($value, ' ');
+
+	return $value;
+}
+
+foreach ($configs as $vm => $config) {
+	echo $vm;
+	echo ', ';
+	foreach ($valueNames as $valueName) {
+		echo $configs[$vm][$valueName];
+		//echo $configs[$vm][$fileValueName];
+		echo ', ';
+	}
+	echo "\n";
+}
