51ed6a58fa768974b97f5ed03f655ed930a08823
Toshihiro Takehara aka nouphet <nouphet.ratanavong@gmail.com>
2013-04-04 02:18:52 +0900

    追加：環境判定、qemu-img の情報をリスト表示

--- get_img_info.php
+++ get_img_info.php
@@ -1,11 +1,38 @@
 <?php
 
+$host = $_SERVER['HTTP_HOST'];
+if ($host === 'localhost') {
+	$env = 'dev';
+} else {
+	$env = 'production';
+}
+
+if ($env === 'dev') {
+	$imgName = `cat xml/takehara-test01.img`;
+	$lines = explode("\n", $imgName);
+	foreach ($lines as $line) {
+		echo "<pre>" . $line . "</pre>";
+	}
+} else {
 	$imgName = `qemu-img info /vm/shiojiri/mobile-dev01.img`;
-	echo $imgName;
-
-	$imgInfo = trim($imgName, "\n");
-	$imgInfo = trim($imgInfo, " ");
-	$imgInfos = explode("\n", $imgInfo);
-
+	$lines = explode("\n", $imgName);
+	foreach ($lines as $line) {
+		echo "<pre>" . $line . "</pre>";
+	}
 	echo $imgInfos;
+}
+
+
+$diskDatas = array();
+foreach ($diskDatas as $diskData) {
+	$imgName        = $diskData[0];
+	$imgFormat      = $diskData[1];
+	$imgVirtualSize = $diskData[2];
+	$imgRealSize    = $diskData[3];
+
+	echo $imgName;
+	echo $imgFormat;
+	echo $imgVirtualSize;
+	echo $imgRealSize;
+}
 
